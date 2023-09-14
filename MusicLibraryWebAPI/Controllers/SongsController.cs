e0d0a27cab82a067968a6026c901dc8c84df12e8
ChaseLoeffler <chaselo609@gmail.com>
2023-09-14 16:25:20 -0400

    Completed and tested Put and Delete Methods.

--- MusicLibraryWebAPI/Controllers/SongsController.cs
+++ MusicLibraryWebAPI/Controllers/SongsController.cs
@@ -50,14 +50,39 @@ namespace MusicLibraryWebAPI.Controllers
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] Song song)
         {
-            return Ok(song);
+           var songU = _context.Songs.FirstOrDefault(s=>s.Id==id);
+            if (songU == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                songU.Artist = song.Artist;
+                songU.Album = song.Album;
+                songU.Genre = song.Genre;
+                songU.Title = song.Title;
+                songU.ReleaseDate = song.ReleaseDate;
+                
+                _context.SaveChanges();
+
+                return StatusCode(200,songU);
+
+            }
+            return BadRequest(ModelState);
         }
 
         // DELETE api/Songs/5
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            return Ok(id);
+            var song = _context.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            _context.Songs.Remove(song);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
